46ca0dbb14c1549ae5cf01e897d34170a8a0058a
wp.nine <wpninestar.wp@gmail.com>
2019-06-23 21:29:13 +0800

    增加‘低级优化’示例代码
    point_test.go
    point.go

--- point.go
+++ point.go
@@ -18,7 +18,7 @@ func GetData() []int {
 	return testData
 }
 
-//创建一个6000000000大小的整型切片
+//创建一个2000000000大小的整型切片
 func CreateTestData()[]int  {
 	data := make([]int,6000000000)
 	for index,_ := range data{
@@ -59,4 +59,56 @@ func toSum4(result *int)  {
 		k += data[i]
 	}
 	*result = k
+}
+
+
+func toSum5(result *int)  {
+	k := *result
+	data := GetData()
+	dataLength := len(data)
+	for i:=1;i< dataLength;i+=2{
+		k += data[i] + data[i - 1]
+	}
+	if dataLength % 2 == 1{
+		k += data[dataLength-1]
+	}
+
+	*result = k
+}
+
+func toSum6(result *int)  {
+	k1 := 0
+	k2 := 0
+	data := GetData()
+	dataLength := len(data)
+
+	for i:=1;i< dataLength;i+=2{
+		k1 += data[i]
+		k2 += data[i - 1]
+	}
+
+	//如果是传入的数量是奇数，则单独对最后一个数进行累加
+	if dataLength % 2 == 1{
+		k1 += data[dataLength-1]
+	}
+
+	*result = k1 + k2
+}
+
+func toSum7(result *int)  {
+	k1 := 0
+	k2 := 0
+	k3 := 0
+
+	data := GetData()
+	dataLength := len(data)
+
+	for i:=0;i< dataLength-3;i+=3{
+		k1 += data[i]
+		k2 += data[i + 1]
+		k3 += data[i + 2]
+	}
+
+
+	*result = k1 + k2 + k3
 }
--- point_test.go
+++ point_test.go
@@ -48,3 +48,22 @@ func BenchmarkData4(b *testing.B)  {
 	b.StopTimer()
 }
 
+func BenchmarkData5(b *testing.B)  {
+	b.ResetTimer()
+
+	var sum *int = new(int)
+	*sum = 0
+
+	toSum5(sum)
+	b.StopTimer()
+}
+
+func BenchmarkData6(b *testing.B)  {
+	b.ResetTimer()
+
+	var sum *int = new(int)
+	*sum = 0
+	toSum6(sum)
+	b.StopTimer()
+}
+
